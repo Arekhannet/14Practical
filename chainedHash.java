ca7972752010093efbf3ee25f26717949c254840
Are_tsh <4493911@myuwc.ac.za>
2026-03-04 14:52:55 +0200

    Implement Node class for chainedHash
    Added a Node class for linked-list implementation in chainedHash.

--- chainedHash.java
+++ chainedHash.java
@@ -1,1 +1,15 @@
+public class chainedHash {
+
+    // Linked-list node for each chain
+    private static class Node {
+        String key;
+        String value;
+        Node   next;
+        Node(String key, String value) {
+            this.key = key; this.value = value; this.next = null;
+        }
+    }
+
+  
+}
 
